f4c0da995f3a870da270ffe28bab4a1928cf9027
Yegor Kuzovkov <54142240+OriginsOfFire@users.noreply.github.com>
2021-02-27 00:36:18 +0300

    Upgrade python script

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import numpy as np
+import random as rnd
 def main():
-  pass
+  size = rnd.randint(3, 7)
+  a = np.array(size, size)
+  b = np.array(size, size)
+  
+  for i in range(0, size):
+    for j in range(0, size):
+      a[i][j] = rnd.randint(0, 10)
+      b[i][j] = rnd.randint(0, 10)
+  
+  res = a.dot(b)
+  print(res)
 
 if __name__ == "__main__":
   main()
