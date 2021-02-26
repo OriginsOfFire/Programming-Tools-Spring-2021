7b9e7b3bc3be9fafbe075503e63efb12a22fe42d
Yegor <egkuzoff@gmail.com>
2021-02-27 00:49:02 +0300

    Fixed numpy arrays

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import numpy as np
 import random as rnd
 def main():
   size = rnd.randint(3, 7)
-  a = np.zeros(size, int)
-  b = np.zeros(size, int)
+  a = [[size], [size]]
+  b = [[size], [size]]
   
   for i in range(0, size):
     for j in range(0, size):
       a[i][j] = rnd.randint(0, 10)
       b[i][j] = rnd.randint(0, 10)
+      
+  A = np.array(a)
+  A = np.array(b)
   
-  res = a.dot(b)
+  
+  
+  res = A.dot(B)
   print(res)
 
 if __name__ == "__main__":
