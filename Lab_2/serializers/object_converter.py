1f2bf7fc80de8a270c97c3b13556abf483bbd614
BoyNextDoorxBY <egkuzoff@gmail.com>
2021-05-28 23:53:05 +0300

    Finish serializers

--- Lab_2/serializers/object_converter.py
+++ Lab_2/serializers/object_converter.py
@@ -1,84 +1,96 @@
 import inspect
 import builtins
 from types import CodeType, FunctionType
+from collections import namedtuple
 
-PRIMITIVES = [int, float, str, bool, list, tuple, type(None)]
 
+class Converter:
+    @staticmethod
+    def __is_primitive(obj):
+        return type(obj) in [int, float, bool, str, type(None)]
 
-def object_to_dict(obj):
-    res = {}
-    if type(obj) is list:
-        counter = 1
-        for item in obj:
-            res["Variable" + str(counter)] = item
-            counter += 1
+    def __dict_converter(self, obj):
+        res = {}
+        for key, value in obj.items():
+            if self.__is_primitive(value):
+                res[key] = value
+            else:
+                res[key] = self.object_to_dict(value)
         return res
-    elif type(obj) is dict:
-        return obj
-    elif type(obj) in PRIMITIVES:
-        res["Variable"] = obj
-    else:
-        res += obj.__dict__
-    return res
 
+    def object_to_dict(self, obj):
+        res = {}
+        if self.__is_primitive(obj):
+            return obj
+        elif isinstance(obj, list):
+            res = list()
+            for item in obj:
+                res.append(self.object_to_dict(item))
+            return res
+        elif isinstance(obj, dict):
+            return self.__dict_converter(obj)
+        else:
+            return self.__dict_converter(obj.__dict__)
 
-def dict_to_object(source):
-    keys = source.keys()
-    if "Variable" in keys:
-        return source["Variable"]
-    elif "Variable1" in keys:
-        res = []
-        for key in keys:
-            res.append(source[key])
+    def dict_to_object(self, dick):
+        if self.__is_primitive(dick):
+            return dick
+        elif isinstance(dick, list):
+            res = []
+            for item in dick:
+                res.append(self.dict_to_object(item))
+            return res
+
+        else:
+            res = {}
+            for key, value in dick.items():
+                if not self.__is_primitive(value):
+                    res[key] = self.dict_to_object(value)
+                else:
+                    res[key] = value
+            return namedtuple('object', res.keys())(*res.values())
+
+    def function_to_dict(self, func):
+        res, code = {}, {}
+        code_pairs = inspect.getmembers(func.__code__)
+        for key, value in code_pairs:
+            if str(key).startswith('co_'):
+                if isinstance(value, bytes):
+                    value = (list(value))
+                code[key] = value
+        res['code'] = code
+
+        globs = {}
+        name = res['code']['co_name']
+        globs[name] = name + '<function>'
+        global_pairs = func.__globals__.items()
+        for key, value in global_pairs:
+            if self.__is_primitive(value):
+                globs[key] = value
+        res['globals'] = globs
         return res
-    elif type(source) is dict:
-        return source
 
+    def dict_to_function(self, dick):
+        globs = dick['globals']
+        globs['__builtins__'] = builtins
+        args = dick['code']
 
-def func_to_dict(func):
-    members = inspect.getmembers(func.__code__)
-    func_dict = {}
-    for item in members:
-        if item[0].startswith('co'):
-            func_dict[item[0]] = item[1]
-    func_dict['co_code'] = list(func_dict['co_code'])
-    func_dict['co_lnotab'] = list(func_dict["co_lnotab"])
-
-    res = {'code' : func_dict}
-    globs = dict()
-    name = func['code']['co_name']
-    globs[name] = name + '<function>'
-    key_values = func.__globals__.items()
-    for (key, value) in key_values:
-        if value in PRIMITIVES:
-            globs[key] = value
-    res['globals'] = globs
-    return res
-
-
-def dict_to_function(source):
-    globs = source['globals']
-    globs['__builtins__'] = builtins
-    code_args = source['code']
-
-    res_obj = CodeType(code_args['co_argcount'],
-                      code_args['co_posonlyargcount'],
-                      code_args['co_kwonlyargcount'],
-                      code_args['co_nlocals'],
-                      code_args['co_stacksize'],
-                      code_args['co_flags'],
-                      bytes(code_args['co_code']),
-                      tuple(code_args['co_consts']),
-                      tuple(code_args['co_names']),
-                      tuple(code_args['co_varnames']),
-                      code_args['co_filename'],
-                      code_args['co_name'],
-                      code_args['co_firstlineno'],
-                      bytes(code_args['co_lnotab']),
-                      tuple(code_args['co_freevars']),
-                      tuple(code_args['co_cellvars']))
-
-    temp = FunctionType(res_obj, globs, code_args['co_name'])
-    name = code_args['co_name']
-    globs[name] = temp
-    return FunctionType(res_obj, globs, name)
+        code = CodeType(args['co_argcount'],
+                                 args['co_posonlyargcount'],
+                                 args['co_kwonlyargcount'],
+                                 args['co_nlocals'],
+                                 args['co_stacksize'],
+                                 args['co_flags'],
+                                 bytes(args['co_code']),
+                                 tuple(args['co_consts']),
+                                 tuple(args['co_names']),
+                                 tuple(args['co_varnames']),
+                                 args['co_filename'],
+                                 args['co_name'],
+                                 args['co_firstlineno'],
+                                 bytes(args['co_lnotab']),
+                                 tuple(args['co_freevars']),
+                                 tuple(args['co_cellvars']))
+        temp = FunctionType(code, globs, args['co_name'])
+        name = args['co_name']
+        return FunctionType(code, globs, name)
